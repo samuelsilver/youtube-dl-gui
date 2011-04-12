75909162bf79e4a55393b16fb9e35556a3bc2a18
fredy.wijaya@gmail.com <fredy.wijaya@gmail.com@eb2b057a-4ec6-3ae2-0a6d-6aa795b576d6>
2011-04-12 08:37:57 +0000

    - Added proper LGPL license

--- trunk/youtube-dl-gui.py
+++ trunk/youtube-dl-gui.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2011, Fredy Wijaya
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the Lesser GNU General Public License as published by
+# the Free Software Foundation, either version 3.0 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Lesser GNU General Public License for more details.
+#
+# You should have received a copy of the Lesser GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>
 
 import wx, sys, threading, os, subprocess, cStringIO, youtubedl
 from wx.lib.mixins.listctrl import TextEditMixin
